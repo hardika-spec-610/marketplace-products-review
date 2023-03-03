2fd6d7639b6fa087818a2789c0659e28ae77086b
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-03-03 12:07:16 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import Express from "express"; // NEW IMPORT SYNTAX (We can use it only if we add "type": "module", to package.json)
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import {
+  genericErrorHandler,
+  badRequestHandler,
+  unauthorizedHandler,
+  notfoundHandler,
+} from "./errorsHandlers.js";
+
+const server = Express();
+const port = 3001;
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(
+    `Request method ${req.method} -- url ${req.url} -- ${new Date()}`
+  );
+  req.user = "Hardika";
+  next();
+};
+
+server.use(cors());
+server.use(loggerMiddleware);
+server.use(Express.json());
+
+server.listen(port, () => {
+  console.table(listEndpoints(server));
+  console.log(`Server is running on port ${port}`);
+});
+
+server.use(badRequestHandler); // 400
+server.use(unauthorizedHandler); // 401
+server.use(notfoundHandler); // 404
+server.use(genericErrorHandler); // 500 (this should ALWAYS be the last one)
+
+server.on("error", (error) =>
+  console.log(`Server is not running due to: ${error}`)
+);
