7608f307cf5c89251f2c40abd550453d2bf528f4
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-03-17 13:06:43 +0100

    put, delete and get specific product method

--- src/api/products/productIndex.js
+++ src/api/products/productIndex.js
@@ -24,7 +24,7 @@ productRouter.get("/", async (req, res, next) => {
     console.log("q2m", q2m(req.query));
     const mongoQuery = q2m(req.query);
     //  price: '>10' should be converted somehow into price: {$gt: 10}
-    const blogs = await ProductsModel.find(
+    const products = await ProductsModel.find(
       mongoQuery.criteria,
       mongoQuery.options.fields
     )
@@ -38,8 +38,55 @@ productRouter.get("/", async (req, res, next) => {
       links: mongoQuery.links(process.env.LOCAL_URL + "/products", total),
       total,
       numberOfPages: Math.ceil(total / mongoQuery.options.limit),
-      blogs,
+      products,
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.get("/:id", async (req, res, next) => {
+  try {
+    const products = await ProductsModel.findById(req.params.id).populate({
+      path: "reviews",
+      select: "comment rate",
+    });
+    if (products) {
+      res.send(products);
+    } else {
+      next(createHttpError(404, `Product with id ${req.params.id} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.put("/:id", async (req, res, next) => {
+  try {
+    const updatedProduct = await ProductsModel.findByIdAndUpdate(
+      req.params.id, // WHO
+      req.body, // HOW
+      { new: true, runValidators: true } // OPTIONS. By default findByIdAndUpdate returns the record pre-modification. If you want to get the newly updated one you shall use new: true
+      // By default validation is off in the findByIdAndUpdate --> runValidators: true
+    );
+    if (updatedProduct) {
+      res.send(updatedProduct);
+    } else {
+      next(createHttpError(404, `Product with id ${req.params.id} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const deletedProduct = await ProductsModel.findByIdAndDelete(req.params.id);
+    if (deletedProduct) {
+      res.status(204).send();
+    } else {
+      next(createHttpError(404, `Product with id ${req.params.id} not found!`));
+    }
   } catch (error) {
     next(error);
   }
